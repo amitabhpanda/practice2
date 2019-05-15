c1cc18f1038d8415c6743340c45cdbb1adb7fb49
amitabh panda <amitabhpanda.19@gmail.com>
2019-05-15 22:53:37 +0530

    commiting for the second time 	new file:   robofriends/.gitignore 	new file:   robofriends/README.md 	new file:   robofriends/package-lock.json 	new file:   robofriends/package.json 	new file:   robofriends/public/favicon.ico 	new file:   robofriends/public/index.html 	new file:   robofriends/public/manifest.json 	new file:   robofriends/src/App.css 	new file:   robofriends/src/App.js 	new file:   robofriends/src/App.test.js 	new file:   robofriends/src/Hello.js 	new file:   robofriends/src/index.css 	new file:   robofriends/src/index.js 	new file:   robofriends/src/logo.svg 	new file:   robofriends/src/serviceWorker.js

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Hello extends Component{
+	render()
+	{
+		return(
+			<div>
+				<h1>Hello world</h1>
+				<h2>Welcome to the app</h2>
+			</div>
+			);
+	}
+}
+
+export default Hello;
